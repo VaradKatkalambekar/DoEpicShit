169f305077939b5ae63820c474a6fa58df2c4211
Varad Katkalambekar <88434682+VaradKatkalambekar@users.noreply.github.com>
2021-11-08 14:20:06 +0530

    Update DoEpicShit.py

--- DoEpicShit.py
+++ DoEpicShit.py
@@ -16,4 +16,4 @@ hours_rem = 365*16 - ((m-1)*30*16 + d*16)
 
 print('In this month you have ',time_month,'% time remaining for your productive work!')
 print('in this year',y,', you have ',int(time_year),'% time remaining for your work')
-print('In total you have ',hours_rem,'hours, equal to seeing ',int(hours_rem/3),'movies of 3hrs or complete',int(hours_rem/24),'days for your work!! Get up and do some shit.')
+print('In total you have ',hours_rem,'hours, equal to seeing 3 idiots',int(hours_rem/3),'times or complete',int(hours_rem/24),'days for your work!! Get up and do some shit.')
